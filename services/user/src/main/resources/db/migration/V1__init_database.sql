48f92cba1f7a939746d36061388a7411d50cbb36
Almor21 <eanoriega@uninorte.edu.co>
2025-10-25 01:22:38 -0500

    feat: create init database migration to create users table

---
+++ services/user/src/main/resources/db/migration/V1__init_database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE if not exists users (
+    id VARCHAR(36) PRIMARY KEY DEFAULT gen_random_uuid()::text,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
