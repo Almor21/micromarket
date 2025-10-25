eaa4474690e9ff01c473685fd92ab2ac682784e7
Almor21 <eanoriega@uninorte.edu.co>
2025-10-25 01:16:07 -0500

    feat: create CreateUserMessage in common package

---
+++ common/src/main/java/com/almor/common/message/CreateUserMessage.java
@@ -0,0 +1,24 @@
+package com.almor.common.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateUserMessage {
+
+    private String id;
+
+    private String username;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+}
