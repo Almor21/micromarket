a9fff5e8e940fcd2939b626855263d42f011eb71
Almor21 <eanoriega@uninorte.edu.co>
2025-10-25 01:26:05 -0500

    feat: create user controller and add routes

---
+++ services/user/src/main/java/com/almor/user/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.almor.user.controller;
+
+import com.almor.user.mapper.UserMapper;
+import com.almor.user.dto.request.CreateUserDto;
+import com.almor.user.dto.response.UserResponseDto;
+import com.almor.user.dto.request.UpdateUserDto;
+import com.almor.user.entity.User;
+import com.almor.user.message.CreateUserMessage;
+import com.almor.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @PostMapping
+    public UserResponseDto createUser(@Valid @RequestBody CreateUserDto data) {
+        User user = userService.create(data);
+        return UserMapper.toResponse(user);
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDto updateUser(@PathVariable String id, @Valid @RequestBody UpdateUserDto data) {
+        User user = userService.update(id, data);
+        return UserMapper.toResponse(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.delete(id);
+    }
+}
