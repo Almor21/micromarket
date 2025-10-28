077fa35b1e6c610423a050e6a6c857829683e544
Almor21 <eanoriega@uninorte.edu.co>
2025-10-28 15:07:33 -0500

    fix[user-service]: add ResponseEntity to endpoint responses

--- services/user/src/main/java/com/almor/user/controller/UserController.java
+++ services/user/src/main/java/com/almor/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.almor.user.controller;
 
+import com.almor.user.dto.response.UserResponseDto;
 import com.almor.user.entity.User;
+import com.almor.user.mapper.UserMapper;
 import com.almor.user.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,14 +18,23 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<User> getAllUsers() {
-        return userService.findAll();
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<User> users = userService.findAll();
+        List<UserResponseDto> response = users.stream()
+                .map(UserMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(response);
+
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable String id) {
-        return userService.findById(id)
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable String id) {
+        User user = userService.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
+        UserResponseDto response = UserMapper.toResponse(user);
+
+        return ResponseEntity.ok(response);
     }
 
 }
